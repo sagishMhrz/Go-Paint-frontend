5af7cdd914f9c65af499c2ca209f37b3f0007c30
sagishMhrz <sagishmhrz888@gmail.com>
2026-06-20 19:32:45 +0545

    painter login

--- GoPaint/src/components/user/Layout.jsx
+++ GoPaint/src/components/user/Layout.jsx
@@ -1,25 +1,19 @@
-import { useEffect, useState } from "react";
 import { Outlet } from "react-router";
 import { useLocation } from "react-router-dom";
-import Header, { isUserLoggedIn } from "./Header";
+import Header from "./Header";
 import Footer from "./Footer";
 
 const Layout = () => {
   const { pathname } = useLocation();
-  const [loggedIn, setLoggedIn] = useState(isUserLoggedIn);
   const forceScrolledHeader =
     pathname === "/colors" ||
     pathname === "/find-painters" ||
     pathname === "/how-it-works" ||
     pathname.startsWith("/hire-painter");
 
-  useEffect(() => {
-    setLoggedIn(isUserLoggedIn());
-  }, [pathname]);
-
   return (
     <div className="flex min-h-screen flex-col bg-white">
-      <Header forceScrolled={forceScrolledHeader} isLoggedIn={loggedIn} />
+      <Header forceScrolled={forceScrolledHeader} />
       <main className="flex-1">
         <Outlet />
       </main>
